465655c6d08491b58f234bd9a6d04beaa991c9ab
Ahmadhere1 <aghazi1011@gmail.com>
2022-06-25 09:51:05 +0500

    sixth

---
+++ src/screens/OrderComplete.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const OrderComplete = () => {
+    return (
+        <View>
+            <Text>OrderComplete</Text>
+        </View>
+    )
+}
+
+export { OrderComplete }
+
+const styles = StyleSheet.create({
+
+})
